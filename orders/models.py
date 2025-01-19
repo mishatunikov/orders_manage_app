2c9d762bfac7786d712904bdbc795ae5dcef5cc5
mishatunikov <michael.tunikov@gmail.com>
2025-01-19 21:22:32 +0300

    Создана базовая модель заказа.

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    STATUSES = {
+        'waiting': 'В ожидании',
+        'ready': 'Готово',
+        'paid': 'Оплачено'
+    }
+    table_number = models.IntegerField(verbose_name='Номер стола',
+                                       help_text='Номер стола')
+    items = models.JSONField(verbose_name='Блюда')
+    status = models.CharField(
+        'Status',
+        max_length=25,
+        choices=STATUSES,
+        default=STATUSES['waiting'],
+    )
+    total_price = models.IntegerField(verbose_name='Стоимость заказа')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    def __str__(self):
+        return f'Заказ №{self.pk}'
+
+
+
+
+
