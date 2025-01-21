ccceb77fe03b94f1e366ff15c8e999e56d830fff
mishatunikov <michael.tunikov@gmail.com>
2025-01-21 16:08:04 +0300

    Добавлена сортировка по-умолчанию, новое свойство.

--- orders/models.py
+++ orders/models.py
@@ -19,6 +19,13 @@ class Order(models.Model):
     total_price = models.IntegerField(verbose_name='Стоимость заказа')
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
 
+    @property
+    def items_names(self) -> list:
+        return [item['name'] for item in self.items]
+
+    class Meta:
+        ordering = ('-created_at', )
+
     def __str__(self):
         return f'Заказ №{self.pk}'
 
