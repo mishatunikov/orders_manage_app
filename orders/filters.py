52b3e203c1bc3b0f2f38cf0c84e8e4510592cf33
mishatunikov <michael.tunikov@gmail.com>
2025-01-22 12:53:07 +0300

    Добавлен модуль фильтров.

---
+++ orders/filters.py
@@ -0,0 +1,15 @@
+import re
+
+
+def is_text(string: str) -> bool:
+    """Проверяет строку на то, состоит ли она только из букв и пробелов."""
+    if re.fullmatch(r'^[а-яА-Яa-zA-Z ]+', string):
+        return True
+
+
+def is_number(string: str) -> bool:
+    """Проверяет строку на то, является ли она корректным числом."""
+    if re.fullmatch(r'[1-9][0-9]*', string):
+        return True
+
+
