a27310392933064c39bb16bfab375a916bc820f6
mishatunikov <michael.tunikov@gmail.com>
2025-01-22 13:07:06 +0300

    STATUSES вынесено в constants.py.

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
+from orders.contants import STATUSES
+
 
 class Order(models.Model):
-    STATUSES = {
-        'waiting': 'В ожидании',
-        'ready': 'Готово',
-        'paid': 'Оплачено'
-    }
     table_number = models.IntegerField(verbose_name='Номер стола',
                                        help_text='Номер стола')
     items = models.JSONField(verbose_name='Блюда')
@@ -14,14 +11,14 @@ class Order(models.Model):
         'Status',
         max_length=25,
         choices=STATUSES,
-        default=STATUSES['waiting'],
+        default='waiting',
     )
     total_price = models.IntegerField(verbose_name='Стоимость заказа')
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
 
     @property
-    def items_names(self) -> list:
-        return [item['name'] for item in self.items]
+    def item_name_price(self) -> list:
+        return [(item["name"], item["price"]) for item in self.items]
 
     class Meta:
         ordering = ('-created_at', )
