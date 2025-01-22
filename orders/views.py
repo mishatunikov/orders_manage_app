9a4672813430177efc5568faf1a679af2592e8a0
mishatunikov <michael.tunikov@gmail.com>
2025-01-22 12:51:38 +0300

    Созданы миксины, обработчик страницы статистики. В OrderListView добавлена обработка запросов поиска.

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,112 @@
+from django.db.models import Sum
 from django.views.generic import (
     CreateView,
     ListView,
     DeleteView,
     UpdateView,
     DetailView,
+    TemplateView,
 )
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
-from orders.forms import OrderCreationForm
+from orders.forms import OrderForm
 from orders.models import Order
+from orders.filters import is_text, is_number
+from orders.contants import STATUSES
+
+
+class OrderActionMixin:
+    """Миксин для страниц действий с заказом."""
+    model = Order
+    template_name = 'orders/create.html'
+    success_url = reverse_lazy('orders:homepage')
+
+
+class OrderEditCreateMixin(OrderActionMixin):
+    """Миксин для страниц редактирования и создания заказа."""
+    form_class = OrderForm
+
+    def post(self, request, *args, **kwargs):
+        post_data = request.POST.copy()
+        post_data['items'] = [
+            {'name': item, 'price': price}
+            for item, price in zip(
+                request.POST.getlist('items[]'),
+                request.POST.getlist('prices[]'),
+            )
+        ]
+        if 'status' not in post_data:
+            post_data['status'] = 'waiting'
+        request.POST = post_data
+        return super().post(request, *args, **kwargs)
+
+    def form_invalid(self, form):
+        context = self.get_context_data(form=form)
+        context['error_message'] = form.errors.values()
+        if self.object:
+            context['items'] = zip(
+                self.request.POST.getlist('items[]'),
+                self.request.POST.getlist('prices[]'),
+            )
+        else:
+            context['items'] = zip(
+                self.request.POST.getlist('items[]'),
+                self.request.POST.getlist('prices[]'),
+            )
+        return self.render_to_response(context)
+
+    def form_valid(self, form):
+        form.instance.total_price = sum(
+            item['price'] for item in form.instance.items
+        )
+        return super().form_valid(form)
+
+
+class StatisticView(TemplateView):
+    """Страница со статистикой работы."""
+    template_name = 'orders/statistics.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['paid_orders'] = Order.objects.filter(status='paid')
+        if context['paid_orders']:
+            context['profit'] = context['paid_orders'].aggregate(
+                total_price_sum=Sum('total_price')
+            )['total_price_sum']
+        else:
+            context['profit'] = 0
+        context['waiting_orders'] = Order.objects.filter(status='waiting')
+        context['ready_orders'] = Order.objects.filter(status='ready')
+        return context
+
+
+class OrderCreateView(OrderEditCreateMixin, CreateView):
+    """Создания нового заказа."""
+
+    pass
+
+
+class OrderDeleteView(OrderActionMixin, DeleteView):
+    """Удаление заказа."""
+
+    pass
+
+
+class OrderUpdateView(OrderEditCreateMixin, UpdateView):
+    """Редактирование заказа."""
+
+    def get_success_url(self):
+        return reverse(
+            'orders:detail', kwargs={self.pk_url_kwarg: self.object.pk}
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        print([(item['name'], item['price']) for item in self.object.items])
+        context['items'] = [
+            (item['name'], item['price']) for item in self.object.items
+        ]
+        return context
 
 
 class OrderListView(ListView):
@@ -22,52 +120,23 @@ class OrderListView(ListView):
         context['page_obj'] = context['object_list']
         return context
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        flipped_statuses = {val.lower(): key for key, val in STATUSES.items()}
+        if search := self.request.GET.get('search'):
+            if is_text(search):
+                queryset = queryset.filter(
+                    status=flipped_statuses.get(search.lower())
+                )
 
-class OrderDeleteView(DeleteView):
-    model = Order
-    success_url = reverse_lazy('orders/create.html')
+            elif is_number(search):
+                queryset = queryset.filter(table_number=int(search))
 
-
-class OrderUpdateView(UpdateView):
-    pass
-
-
-class OrderCreateView(CreateView):
-    """Создания нового заказа."""
-
-    model = Order
-    form_class = OrderCreationForm
-    template_name = 'orders/create.html'
-    success_url = reverse_lazy('orders:homepage')
-
-    def form_valid(self, form):
-        form.instance.total_price = sum(
-            item['price'] for item in form.instance.items
-        )
-        return super().form_valid(form)
-
-    def post(self, request, *args, **kwargs):
-        post_data = request.POST.copy()
-        post_data['items'] = [
-            {'name': item, 'price': price}
-            for item, price in zip(
-                request.POST.getlist('items[]'),
-                request.POST.getlist('prices[]'),
-            )
-        ]
-        request.POST = post_data
-        return super().post(request, *args, **kwargs)
-
-    def form_invalid(self, form):
-        context = self.get_context_data(form=form)
-        context['error_message'] = form.errors.values()
-        context['items'] = zip(
-            self.request.POST.getlist('items[]'),
-            self.request.POST.getlist('prices[]'),
-        )
-        return self.render_to_response(context)
+        return queryset
 
 
 class OrderDetailView(DetailView):
+    """Страница отдельного заказа."""
+
     template_name = 'orders/detail.html'
     model = Order
