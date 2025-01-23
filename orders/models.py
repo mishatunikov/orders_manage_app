f967a1658e21abbbe6ce6058b8fb3f0300ec6483
mishatunikov <michael.tunikov@gmail.com>
2025-01-23 14:00:29 +0300

    Добавлены имена и описания для отображения в админке.

--- orders/models.py
+++ orders/models.py
@@ -4,29 +4,48 @@ from orders.contants import STATUSES
 
 
 class Order(models.Model):
-    table_number = models.IntegerField(verbose_name='Номер стола',
-                                       help_text='Номер стола')
-    items = models.JSONField(verbose_name='Блюда')
+    """Модель заказа."""
+
+    table_number = models.IntegerField(
+        verbose_name='Номер стола',
+        help_text='Номер стола ' '(целочисленное значение)',
+    )
+    items = models.JSONField(
+        verbose_name='Блюда',
+        help_text='Поле для хранения заказанных блюд. '
+        'Формат: '
+        '[{"name":"Название блюда", '
+        '"price": Цена (целочисленное значение)},]',
+        default=list,
+    )
     status = models.CharField(
         'Status',
         max_length=25,
         choices=STATUSES,
         default='waiting',
+        help_text='Выберите из выпадающего списка.',
     )
-    total_price = models.IntegerField(verbose_name='Стоимость заказа')
+    total_price = models.IntegerField(
+        verbose_name='Стоимость заказа',
+        help_text='Вычисляется автоматически',
+        blank=True,
+    )
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) -> None:
+        # Для автоматического расчета стоимости заказа.
         self.total_price = sum(item['price'] for item in self.items)
-        # Вызов родительского метода для сохранения объекта в базе данных
         super().save(*args, **kwargs)
 
     @property
     def item_name_price(self) -> list:
+        """Представляет данные items в удобно для работы в шаблоне виде."""
         return [(item["name"], item["price"]) for item in self.items]
 
     class Meta:
-        ordering = ('-created_at', )
+        ordering = ('-created_at',)
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
 
     def __str__(self):
         return f'Заказ №{self.pk}'
