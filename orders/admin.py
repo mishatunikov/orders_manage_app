abc0d63644cd191aac76dcc2cfdd1cf8abb92e96
mishatunikov <michael.tunikov@gmail.com>
2025-01-23 13:59:44 +0300

    Переопределена модель Order в админке.

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+
+# Переопределяем модель Order в админке.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'order_number',
+        'table_number',
+        'status',
+        'total_price',
+        'created_at',
+    )
+
+    @admin.display(description='Заказ')
+    def order_number(self, obj) -> str:
+        return obj
+
+    list_editable = (
+        'table_number',
+        'status',
+    )
+    search_fields = (
+        'table_number',
+        'status',
+    )
+    list_filter = ('created_at',)
+    list_display_links = ('order_number',)
